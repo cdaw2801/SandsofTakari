3857da9562014286808e3930c952e991ceecc344
cdaw2801 <cdaw2801@mpsedu.org>
2026-06-02 14:53:24 -0500

    version 0.1.2
    Added more idols, changed the materials on the inside of the settlement.  and added more cadavers

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,12 +20,13 @@ public class GameManager : MonoBehaviour
     public Button deathRestartButton;
     public Button doneRestartButton;
     public Button startButton;
-
-    public bool isGameActive = false;
-    public bool moveUp = true;
-
-    bool hasShuttleDissappeared = false;
     
+    public bool isGameActive = false;
+    
+
+    // allows the shuttle to dissappear
+    bool hasShuttleDissappeared = false;
+    // gets necessary objects
     public GameObject Player;
     public GameObject Shuttle;
     public GameObject titleScreen;
@@ -40,14 +41,16 @@ public class GameManager : MonoBehaviour
     {
         if (isGameActive)
         {
+            // brings the time down
             timeLeft -= Time.deltaTime;
            
             if (timeLeft <= 0 && hasShuttleDissappeared == false)
             {
-               
+               //removes the shuttle if the timer is done
                Destroy(GameObject.Find("Shuttle"));
                 timeLeft = 0;
                 hasShuttleDissappeared = true; 
+                // kills you if you're too close to the shuttle
                 if (GameObject.Find("Player").transform.position.x < 45 && hasShuttleDissappeared == true)
                 {
                     GameOver();
@@ -63,10 +66,13 @@ public class GameManager : MonoBehaviour
 
     public void GameOver()
     {
+        //allows you to click things
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+        //activates death screen
         deathRestartButton.gameObject.SetActive(true);
         gameOverText.gameObject.SetActive(true);
+        //stops everything
         waterBar.gameObject.SetActive(false);
         isGameActive = false;
         
@@ -75,6 +81,7 @@ public class GameManager : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+        //activates victory screen
         doneRestartButton.gameObject.SetActive(true);
         victoryText.gameObject.SetActive(true);
         waterBar.gameObject.SetActive(false);
@@ -90,11 +97,14 @@ public class GameManager : MonoBehaviour
 
     public void StartGame()
     {
+        //starts the game
         isGameActive = true;
+        //starts the water bar
         waterBar.gameObject.SetActive(true);
+        //sets the timer to 1 minute
         timeLeft = 60f;
         titleScreen.gameObject.SetActive(false);
-        
+        // uses the mouse to look around
         if (isGameActive)
         {
             Cursor.lockState = CursorLockMode.Locked;
